ac262e8b4027ac4a20788d6664762888583cd745
David Dehoust <dehoustdavid@gmail.com>
2022-09-20 11:31:04 +0200

    ajout du fichier salaryFunction.java

---
+++ salaryFunction.java
@@ -0,0 +1,52 @@
+import java.text.DecimalFormat;
+
+public class salaryFunction {
+
+    public static void main(String[] args) {
+
+        // déclaration des variables
+        boolean bool1 = false;
+
+        float taxPercent = 0.0f;
+        float parsedArgs0 = Float.parseFloat(args[0]);
+
+        // choix du nombre de chiffres après la virgule.
+        DecimalFormat decimalFormatA = new DecimalFormat("#.#");
+        DecimalFormat decimalFormatB = new DecimalFormat("#.##");
+        DecimalFormat decimalFormatC = new DecimalFormat("#.###");
+        // POST-IT : System.out.println(decimalFormatA.format(float1));
+
+        // execution du programme.
+
+        if (args.length > 0) {
+
+            taxPercent = TaxPercentCalc(parsedArgs0);
+            System.out.println("taxe : " + taxPercent + "%" + "\ncalcul : " + (parsedArgs0 - TaxCalculation(parsedArgs0, taxPercent)));
+
+        } else {
+            //System.err.println("Vous devez fournir au moins un paramètre, c'est à dire le nom d'une fonction !");
+            System.err.println("Vous devez fournir un nombre comme paramètre !");
+        }
+
+    }
+
+    // fonction qui calcul la taxe en pourcantage.
+    static float TaxPercentCalc(float _value) {
+        float _taxPercent = 0.0f;
+        if (_value < 13440) {
+            _taxPercent = 25;
+        } else if (_value < 23720) {
+            _taxPercent = 40;
+        } else if (_value < 41060) {
+            _taxPercent = 45;
+        } else {
+            _taxPercent = 50;
+        }
+        return _taxPercent;
+    }
+
+    // fonction qui applique le pourcentage à la valeur entrée.
+    static float TaxCalculation(float _value, float _taxPercent) {
+        return (_value / 100) * _taxPercent;
+    }
+}
